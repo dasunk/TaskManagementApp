dd74615f05954599bce376015d4ca1ee87f77bf5
DasunK <dasunk1995@yahoo.com>
2024-06-06 11:41:59 +0530

    ng generate component components/task-form

---
+++ src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent implements OnInit{
+  tasks: string[] = [];
+  newTask: string = '';
+  constructor() { }
+  ngOnInit(): void {
+  }
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+}
